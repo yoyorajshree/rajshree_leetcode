9668430a9ffd423679c6b00fec01999dd96fc5ff
yoyorajshree <rajshree2107004@gmail.com>
2026-08-14 20:48:07 +0530

    Time: 0 ms (100%), Space: 9.9 MB (26.22%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length())
+        return false;
+
+        vector<int>freq(26,0);
+        for(int i=0;i<s.size();i++)
+        freq[s[i]-'a']++;
+
+        for(int i=0;i<t.size();i++)
+        freq[t[i]-'a']--;
+
+        for(int i=0;i<26;i++)
+        {
+            if(freq[i]!=0)
+            return false;
+        }
+        return true;
+    }
+};
