33311374e02d9ac7db0652a7c736bf0943a5b452
yoyorajshree <rajshree2107004@gmail.com>
2026-08-21 22:27:58 +0530

    Time: 0 ms (100%), Space: 7.7 MB (82.58%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        while(n!=1&&n!=4){
+        int sum=0;
+        while(n>0){
+            int d=n%10;
+            sum=sum+(d*d);
+            n=n/10;
+        }
+        n=sum;
+    }
+    return n==1;
+}
+};
+
+                                                        
